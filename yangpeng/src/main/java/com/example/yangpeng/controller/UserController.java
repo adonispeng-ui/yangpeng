bb9443e2b8f2787c1d653d56cfdf4f8f0d9104d0
yang <yp4444@126.com>
2020-05-28 13:47:09 +0800

    add

--- yangpeng/src/main/java/com/example/yangpeng/controller/UserController.java
+++ yangpeng/src/main/java/com/example/yangpeng/controller/UserController.java
@@ -10,8 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -30,14 +31,15 @@ public class UserController {
      * 时间排序
      * 分页
      * 条件查询
+     *
      * @param pageSize
      * @param pageNumber
      * @return
      */
     @ApiOperation(value = "查看用户信息", notes = "查看用户信息")
     @ApiImplicitParams({
-            @ApiImplicitParam(name="pageSize",value="分页参数（必传）",dataType="int", paramType = "query",example="1"),
-            @ApiImplicitParam(name="pageNumber",value="分页参数（必传）",dataType="int", paramType = "query",example="1")
+            @ApiImplicitParam(name = "pageSize", value = "分页参数（必传）", dataType = "int", paramType = "query", example = "1"),
+            @ApiImplicitParam(name = "pageNumber", value = "分页参数（必传）", dataType = "int", paramType = "query", example = "1")
     })
     @ApiResponses({
             @ApiResponse(code = 500, message = "服务器内部错误"),
@@ -46,19 +48,19 @@ public class UserController {
     @GetMapping(value = "/getUserList")
     @ResponseBody
 
-    public ResultJson getUser(@RequestParam Integer pageSize ,@RequestParam Integer pageNumber){
+    public ResultJson getUser(@RequestParam Integer pageSize, @RequestParam Integer pageNumber) {
         ResultJson json = new ResultJson();
-        if (pageSize==null) {
+        if (pageSize == null) {
             json.setSuccess(false);
             json.setMsg("分页参数pageSize不能为空");
             json.setCode(401);
-            return  json;
+            return json;
         }
-        if (pageNumber==null) {
+        if (pageNumber == null) {
             json.setSuccess(false);
             json.setMsg("分页参数pageNumber不能为空");
             json.setCode(401);
-            return  json;
+            return json;
         }
         try {
             Page<Map<String, Object>> list = userService.getUserList(pageSize, pageNumber);
@@ -78,57 +80,65 @@ public class UserController {
 
     /**
      * 注册用户
+     *
      * @param userEntity
      * @return
      */
     @ApiOperation(value = "用户注册", notes = "用户进行注册")
     @PostMapping(value = "/insertUser")
     @ResponseBody
-    public ResultJson insertUser(@Valid @RequestBody UserEntity userEntity ){
+    public ResultJson insertUser(@Valid @RequestBody UserEntity userEntity) {
         ResultJson json = new ResultJson();
 
-        if (userEntity.getAccountUser()==null) {
+        if (userEntity.getAccountUser() == null) {
             json.setSuccess(false);
             json.setMsg("用户名不能为空");
             json.setCode(401);
-            return  json;
+            return json;
         }
 
-        if (userEntity.getEmailUser()==null) {
+        if (userEntity.getEmailUser() == null) {
             json.setSuccess(false);
             json.setMsg("邮箱不能为空");
             json.setCode(401);
-            return  json;
+            return json;
         }
 
-        if(userEntity.getPasswordUser()==null){
+        if (userEntity.getPasswordUser() == null) {
             json.setSuccess(false);
             json.setMsg("密码不能为空");
             json.setCode(401);
-            return  json;
+            return json;
         }
 
         try {
-            //用户名查id
-            boolean accountUserState =  userService.getUserByAccount(userEntity.getAccountUser());
+            //数组 邮箱 手机 用户名
+            Map<String, String> params = new HashMap();
+            params.put("account", userEntity.getAccountUser());
+            params.put("email", userEntity.getEmailUser());
+            params.put("phone", userEntity.getPhoneUser());
+            //用户查id
+            Map<String, Object> newMap = userService.getUserByAccount(params);
 
-            if(accountUserState == true){//没有用户
+            boolean UserState = (boolean) newMap.get("state");
 
-                Integer state =userService.insertUser(userEntity);
+            if (UserState == true) {//没有用户
 
-                if(state == 1){ //创建成功
+                Integer state = userService.insertUser(userEntity);
+
+                if (state == 1) { //创建成功
                     json.setSuccess(true);
                     json.setCode(200);
                     json.setMsg("创建成功");
-                }else{//创建失败
+                } else {//创建失败
                     json.setSuccess(false);
                     json.setCode(500);
                     json.setMsg("创建失败");
                 }
-            }else{//有用户
+            } else {//有用户
                 json.setSuccess(false);
                 json.setCode(500);
-                json.setMsg("用户已经存在");
+                json.setMsg((String) newMap.get("information"));
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -140,18 +150,45 @@ public class UserController {
         return json;
     }
 
+    /**
+     * 验证邮箱
+     *
+     * @param link
+     * @return
+     */
+    @ApiOperation(value = "验证用户邮箱", notes = "验证用户邮箱")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "link", value = "邮箱连接", dataType = "string", paramType = "query", example = "1")
+    })
+    @GetMapping(value = "/verifyMailbox")
+    @ResponseBody
+    public ResultJson verifyMailbox(String link) {
 
+        ResultJson json = new ResultJson();
 
+        if (link == null) {
+            json.setSuccess(false);
+            json.setMsg("邮箱验证链接不能为空");
+            json.setCode(401);
+            return json;
+        }
+        try {
+            boolean randomCodeState = userService.verifyRandomCode(link);
 
-
-
-
-
-
-
-
-
-
+            if (randomCodeState == true) {
+                json.setSuccess(true);
+                json.setCode(200);
+                json.setMsg("验证成功");
+            } else {
+                json.setSuccess(false);
+                json.setCode(500);
+                json.setMsg("验证失败");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
 
 
 }
